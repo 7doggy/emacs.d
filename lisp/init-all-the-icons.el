9b8ab882421bde6cbabe5af8fbf45d92cded8afc
7doggy <seven.doggy@gmail.com>
2017-09-13 15:52:56 +0800

    'xx'

---
+++ lisp/init-all-the-icons.el
@@ -0,0 +1,4 @@
+;;init-all-the-icons.el
+(require 'all-the-icons)
+
+(provide 'init-all-the-icons)
---
+++ lisp/init-neotree.el
@@ -0,0 +1,7 @@
+(require-package 'neotree)
+(require 'neotree)
+;(setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+
+(global-set-key [f8] 'neotree-toggle)
+
+(provide 'init-neotree)
