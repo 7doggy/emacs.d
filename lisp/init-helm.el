776543e4792f92aba2aee9c711248387701653e8
7doggy <seven.doggy@gmail.com>
2017-09-19 16:55:36 +0800

    ''

--- lisp/init-helm.el
+++ lisp/init-helm.el
@@ -6,6 +6,7 @@
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
 (global-set-key (kbd "C-x b") 'helm-buffers-list)
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
 ;(global-set-key [f5] 'helm-browse-project)
 
 (provide 'init-helm)
