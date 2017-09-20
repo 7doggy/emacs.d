326faa9dee8bc0de805e1f7406bccd0e8b9468e4
7doggy <seven.doggy@gmail.com>
2017-09-20 16:17:22 +0800

    ''

--- lisp/init-helm.el
+++ lisp/init-helm.el
@@ -5,7 +5,10 @@
 
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
-(global-set-key (kbd "C-x b") 'helm-buffers-list)
+;(global-set-key (kbd "C-x b") 'helm-buffers-list)
+(global-set-key (kbd "C-x b") 'helm-mini)
+(setq helm-buffers-fuzzy-matching t
+	  helm-recentf-fuzzy-match    t)
 (global-set-key (kbd "M-y") 'helm-show-kill-ring)
 ;(global-set-key [f5] 'helm-browse-project)
 
